ca61693ecc795d98b9eb852b4a83cd3199a881b4
zemetad <zemetad@gmail.com>
2021-08-11 01:23:01 -0500

    Model Created
    Model Created

---
+++ LGX/LGX/Models/Shelf.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace LGX.Models
+{
+    public class Shelf
+    {
+        public int Id { get; set; }
+        public string PanelNumber { get; set; }
+        public string DeviceType { get; set; }
+        public string Comment { get; set; }
+    }
+
+    public class ShelfDBContext : DbContext
+    {
+        public DbSet<Shelf> Shelves { get; set; }
+
+    }
+}
